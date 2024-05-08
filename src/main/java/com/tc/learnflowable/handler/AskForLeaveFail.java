ee3da14dd83d51f6ddfdb31b0200a841f948c52c
tangcheng <tangcheng_java@foxmail.com>
2024-05-08 15:06:11 +0800

    add content

--- src/main/java/com/tc/learnflowable/handler/AskForLeaveFail.java
+++ src/main/java/com/tc/learnflowable/handler/AskForLeaveFail.java
@@ -13,5 +13,9 @@ public class AskForLeaveFail implements JavaDelegate {
         // 可以抽成公共的组件，比如是发送邮件，通过执行参数传入发邮件需要的内容，这里取出参数直接发
         Set<String> variableNames = execution.getVariableNames();
         log.error("请假失败。。。");
+
+
+        // 如果采用bean方式，可以注入taskService来进行完成这个任务（按理改这样）
+        // taskService.complete(execution.getId(), new HashMap<>());
     }
 }
